b794918d2e4b3ba31a476906027c2267986606e7
Miguel <mlpgmnrdps@gmail.com>
2025-11-15 11:29:17 +0800

    feat: added test code for DB connection

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -2,6 +2,8 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DbTest {
     public static void main(String[] args) {
@@ -9,6 +11,14 @@ public class DbTest {
             Connection conn =
                     DriverManager.getConnection("jdbc:mysql://localhost/crisms_db?" +
                             "user=root&password=p@ssword");
+            Statement statement = conn.createStatement();
+            String query = "SELECT * FROM stock_locations";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + ", " + rs.getString(2) + ", " + rs.getString(3));
+            }
+
+            conn.close();
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
