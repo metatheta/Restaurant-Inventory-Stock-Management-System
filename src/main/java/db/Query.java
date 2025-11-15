9e0c15b776503cf0ef498f13909ec34437cc59e6
Miguel <mlpgmnrdps@gmail.com>
2025-11-16 00:44:44 +0800

    feat: made a Query class

---
+++ src/main/java/db/Query.java
@@ -0,0 +1,80 @@
+package db;
+
+public class Query {
+    /*
+        ###############
+        # section 3.0 #
+        ###############
+    */
+    public String stockItemAndSuppliers()
+    {
+        return "";
+    }
+
+    public String storedItemAndLocations()
+    {
+        return "";
+    }
+
+    public String locationAndStoredItems()
+    {
+        return "";
+    }
+
+    public String supplierAndProducts()
+    {
+        return "";
+    }
+
+    /*
+        ###############
+        # section 4.0 #
+        ###############
+    */
+
+    public String restockingItem()
+    {
+        return "";
+    }
+
+    public String buyStockItem(String name, String unitOfMeasure, String category)
+    {
+        return "";
+    }
+
+    public String disposeUnusedStock()
+    {
+        return "";
+    }
+
+    public String createDish(int quantity)
+    {
+        return "";
+    }
+
+    /*
+        ###############
+        # section 5.0 #
+        ###############
+    */
+
+    public String preferredSuppliersReport()
+    {
+        return "";
+    }
+
+    public String storageDistributionReport()
+    {
+        return "";
+    }
+
+    public String seasonalStockReport()
+    {
+        return "";
+    }
+
+    public String expiryReport()
+    {
+        return "";
+    }
+}
