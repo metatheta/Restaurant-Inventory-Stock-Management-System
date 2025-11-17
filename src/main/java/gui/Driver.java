4aa43381867f88883896f1bddf191dea2317b9ff
allen <allen_hizon@dlsu.edu.ph>
2025-11-17 19:16:33 +0800

    feat: changed table font

--- src/main/java/gui/Driver.java
+++ src/main/java/gui/Driver.java
@@ -2,7 +2,6 @@ package gui;
 
 import gui.view.ScreenManager;
 import javafx.application.Application;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
@@ -11,7 +10,6 @@ import java.sql.DriverManager;
 public class Driver extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Font.loadFont(getClass().getResourceAsStream("/fonts/GeistMono-Regular.ttf"), 14);
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/crisms_db?" +
                 "user=root&password=p@ssword"); ;
         ScreenManager.SINGLETON.start(primaryStage, conn);
