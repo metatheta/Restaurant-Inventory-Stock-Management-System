ff3c2382303276a037a2b4ff7f063d32b30adfdf
allen <allen_hizon@dlsu.edu.ph>
2025-11-20 01:37:10 +0800

    feat: reports menu skeleton

--- src/main/java/gui/controllers/ReportsMenuController.java
+++ src/main/java/gui/controllers/ReportsMenuController.java
@@ -1,4 +1,30 @@
 package gui.controllers;
 
+import gui.ScreenManager;
+
 public class ReportsMenuController {
+
+    public void preferredSuppliers() {
+
+    }
+
+    public void storageTypeDistribution() {
+
+    }
+
+    public void seasonalStock() {
+
+    }
+
+    public void expiryAndWaste() {
+
+    }
+
+    public void returnToMainMenu() {
+        try {
+            ScreenManager.SINGLETON.displayScreen("/gui/view/main-menu.fxml");
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
